5b501e5e27a36d9e3a1d9751363ecee33707c5b6
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2021-09-06 23:07:59 +0100

    function addTodo

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,6 +1,36 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+
+  // remove current DOM in the list
+  //list.replaceChildren();
+  // create a new li foe each todo, then add it to the DOM as a child of the list
+  //todos.map(createTodoLi).forEach((li) => list.appendChild(li));
+
+  todos.forEach(addTodo);
+}
+
+function addTodo(todo) {
+  let list = document.querySelector("#todo-list");
+  console.lod(todo.task);
+
+  let li = document.createElement("li");
+
+  li.innerText = todo.task;
+  list.appendChild(li);
+  li.className = "list-group-item";
+
+  let span = document.createElement("span");
+  span.appendChild(li);
+  let check = document.createElement("i");
+  check.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+  check.appendChild(span);
+  let trash = document.createElement("i");
+  trash.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+  trash.appendChild(span);
+
+  console.log(check);
+  span.style.backgroundColor = "blue";
 }
 
 // These are the same todos that currently display in the HTML
